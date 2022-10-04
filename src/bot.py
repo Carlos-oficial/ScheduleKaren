aa4561b2f02dd534dc43f6fe65907b602172cd98
Gustavo Pereira <carlosgsp39@gmail.com>
2022-10-04 17:33:14 +0100

    Base bot

--- src/bot.py
+++ src/bot.py
@@ -1,21 +1,20 @@
 from db import db as db
+
+import os
+
 import discord
+from dotenv import load_dotenv
 
-bot = discord.Client()
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
 
-@bot.event
+intents = discord.Intents.default()
+intents.message_content = False
+
+client = discord.Client(intents=intents)
+
+@client.event
 async def on_ready():
-	guild_count = 0
+    print(f'{client.user} has connected to Discord!')
 
-	for guild in bot.guilds:
-		print(f"- {guild.id} (name: {guild.name})")
-		guild_count = guild_count + 1
-
-	print("SampleDiscordBot is in " + str(guild_count) + " guilds.")
-
-@bot.event
-async def on_message(message):
-	if message.content == "hello":
-		await message.channel.send("hey dirtbag")
-
-bot.run("MTAyNjg1NjE4NjE0ODAzNjY2OQ.GipG7r.i5cM1Mpr-n6WOa-6jQS2haiJ_Iza9SfCAEstRI")
+client.run(TOKEN)
